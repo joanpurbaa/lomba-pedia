fb3eaad5b6bc7f444ce528d41b47fb1d6cd0e11f
joanpurbaa <joanpurba562@gmail.com>
2024-09-16 23:41:43 +0700

    Feat : add feature that can get data from form

--- frontend/src/pages/UnggahLomba.jsx
+++ frontend/src/pages/UnggahLomba.jsx
@@ -1,8 +1,41 @@
-import { Button, Label, FileInput, TextInput, Textarea } from "flowbite-react";
+import { useState } from "react";
+import {
+  Button,
+  Label,
+  FileInput,
+  TextInput,
+  Textarea,
+  Datepicker,
+} from "flowbite-react";
 import { Link } from "react-router-dom";
 import { IoCallOutline } from "react-icons/io5";
 
 const UnggahLomba = () => {
+  const [poster, setposter] = useState();
+  const [nama, setNama] = useState();
+  const [Penyelenggara, setPenyelenggara] = useState();
+  const [deadline, setDeadline] = useState();
+  const [linkPendaftaran, setLinkPendaftaran] = useState();
+  const [narahubung, setNarahubung] = useState();
+  const [tingkat, setTingkat] = useState();
+  const [peserta, setPeserta] = useState();
+  const [biaya, setBiaya] = useState();
+  const [deskripsi, setDeskripsi] = useState();
+
+  const setData = (e) => {
+    e.preventDefault();
+
+    console.log(nama);
+    console.log(Penyelenggara);
+    console.log(deadline);
+    console.log(linkPendaftaran);
+    console.log(narahubung);
+    console.log(tingkat);
+    console.log(peserta);
+    console.log(biaya);
+    console.log(deskripsi);
+  };
+
   return (
     <>
       <div className="min-h-dvh items-center text-white p-5">
@@ -17,19 +50,24 @@ const UnggahLomba = () => {
             </Button>
           </Link>
         </div>
-        <form className="grid grid-cols-12 gap-x-5 gap-y-5 mt-5">
+        <form
+          className="grid grid-cols-12 gap-x-5 gap-y-5 mt-5"
+          onSubmit={setData}
+        >
           <div className="col-span-4 flex flex-col gap-y-5 w-full sm:h-[89vh]">
             <h2 className="text-xl font-semibold">Unggah poster lomba</h2>
             <Label
               htmlFor="dropzone-file"
-              className="flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 hover:bg-zinc-800">
+              className="flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 hover:bg-zinc-800"
+            >
               <div className="flex flex-col items-center justify-center pb-6 pt-5">
                 <svg
                   className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
-                  viewBox="0 0 20 16">
+                  viewBox="0 0 20 16"
+                >
                   <path
                     stroke="currentColor"
                     strokeLinecap="round"
@@ -57,18 +95,22 @@ const UnggahLomba = () => {
                   Nama lomba
                 </label>
                 <TextInput
+                  onChange={(e) => setNama(e.target.value)}
                   color="default"
                   className="mt-3"
-                  placeholder="Lomba Web Design Universitas..."></TextInput>
+                  placeholder="Lomba Web Design Universitas..."
+                ></TextInput>
               </li>
               <li>
                 <label className="font-semibold" htmlFor="">
                   Penyelenggara lomba
                 </label>
                 <TextInput
+                  onChange={(e) => setPenyelenggara(e.target.value)}
                   color="default"
                   className="mt-3"
-                  placeholder="Universitas..."></TextInput>
+                  placeholder="Universitas..."
+                ></TextInput>
               </li>
               <li>
                 <label className="font-semibold" htmlFor="">
@@ -77,19 +119,20 @@ const UnggahLomba = () => {
                 <p className="text-gray-400 text-sm">
                   Lomba akan dihapus tepat deadline pendaftaran ditutup
                 </p>
-                <TextInput
-                  color="default"
-                  className="mt-3"
-                  placeholder="1 Oktober 2024..."></TextInput>
+                <Datepicker
+                  onChange={(e) => setDeadline(e.target.value)}
+                ></Datepicker>
               </li>
               <li>
                 <label className="font-semibold" htmlFor="">
                   Link pendaftaran lomba
                 </label>
                 <TextInput
+                  onChange={(e) => setLinkPendaftaran(e.target.value)}
                   color="default"
                   className="mt-3"
-                  placeholder="https://dafarlombaposter..."></TextInput>
+                  placeholder="https://dafarlombaposter..."
+                ></TextInput>
               </li>
               <li>
                 <label className="font-semibold" htmlFor="">
@@ -99,9 +142,11 @@ const UnggahLomba = () => {
                   Link narahubung berupa (WhatsApp/Telegram/Instagram)
                 </p>
                 <TextInput
+                  onChange={(e) => setNarahubung(e.target.value)}
                   color="default"
                   className="mt-3"
-                  placeholder="https://wa.me/628**********"></TextInput>
+                  placeholder="https://wa.me/628**********"
+                ></TextInput>
               </li>
               <li>
                 <label className="font-semibold" htmlFor="">
@@ -109,23 +154,53 @@ const UnggahLomba = () => {
                 </label>
                 <div className="flex flex-wrap gap-x-5 gap-y-2 mt-3">
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="internasional" type="radio" />
+                    <input
+                      onChange={() => setTingkat("Internasional")}
+                      className="rounded-full"
+                      name="level"
+                      id="internasional"
+                      type="radio"
+                    />
                     <label htmlFor="internasional">Internasional</label>
                   </div>
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="nasional" type="radio" />
+                    <input
+                      onChange={() => setTingkat("Nasional")}
+                      className="rounded-full"
+                      name="level"
+                      id="nasional"
+                      type="radio"
+                    />
                     <label htmlFor="nasional">Nasional</label>
                   </div>
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="provinsi" type="radio" />
+                    <input
+                      onChange={() => setTingkat("Provinsi")}
+                      className="rounded-full"
+                      name="level"
+                      id="provinsi"
+                      type="radio"
+                    />
                     <label htmlFor="provinsi">Provinsi</label>
                   </div>
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="kabupaten" type="radio" />
+                    <input
+                      onChange={() => setTingkat("Kabupaten/Kota")}
+                      className="rounded-full"
+                      name="level"
+                      id="kabupaten"
+                      type="radio"
+                    />
                     <label htmlFor="kabupaten">Kabupaten/Kota</label>
                   </div>
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="umum" type="radio" />
+                    <input
+                      onChange={() => setTingkat("Umum")}
+                      className="rounded-full"
+                      name="level"
+                      id="umum"
+                      type="radio"
+                    />
                     <label htmlFor="umum">Umum</label>
                   </div>
                 </div>
@@ -136,23 +211,53 @@ const UnggahLomba = () => {
                 </label>
                 <div className="flex flex-wrap gap-x-5 gap-y-2 mt-3">
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="mahasiswa" type="checkbox" />
+                    <input
+                      onChange={() => setPeserta("Mahasiswa")}
+                      className="rounded-full"
+                      name="level"
+                      id="mahasiswa"
+                      type="checkbox"
+                    />
                     <label htmlFor="mahasiswa">Mahasiswa</label>
                   </div>
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="sma-smk" type="checkbox" />
+                    <input
+                      onChange={() => setPeserta("SMA Sederajat")}
+                      className="rounded-full"
+                      name="level"
+                      id="sma-smk"
+                      type="checkbox"
+                    />
                     <label htmlFor="sma-smk">SMA Sederajat</label>
                   </div>
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="smp" type="checkbox" />
+                    <input
+                      onChange={() => setPeserta("SMP")}
+                      className="rounded-full"
+                      name="level"
+                      id="smp"
+                      type="checkbox"
+                    />
                     <label htmlFor="smp">SMP</label>
                   </div>
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="sd" type="checkbox" />
+                    <input
+                      onChange={() => setPeserta("SD")}
+                      className="rounded-full"
+                      name="level"
+                      id="sd"
+                      type="checkbox"
+                    />
                     <label htmlFor="sd">SD</label>
                   </div>
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="umum" type="checkbox" />
+                    <input
+                      onChange={() => setPeserta("Umum")}
+                      className="rounded-full"
+                      name="level"
+                      id="umum"
+                      type="checkbox"
+                    />
                     <label htmlFor="umum">Umum</label>
                   </div>
                 </div>
@@ -163,11 +268,23 @@ const UnggahLomba = () => {
                 </label>
                 <div className="flex flex-wrap gap-x-5 gap-y-2 mt-3">
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="berbayar" type="radio" />
+                    <input
+                      onChange={() => setBiaya("Berbayar")}
+                      className="rounded-full"
+                      name="level"
+                      id="berbayar"
+                      type="radio"
+                    />
                     <label htmlFor="berbayar">Berbayar</label>
                   </div>
                   <div className="flex items-center gap-x-2">
-                    <input className="rounded-full" name="level" id="gratis" type="radio" />
+                    <input
+                      onChange={() => setBiaya("Gratis")}
+                      className="rounded-full"
+                      name="level"
+                      id="gratis"
+                      type="radio"
+                    />
                     <label htmlFor="gratis">Gratis</label>
                   </div>
                 </div>
@@ -177,13 +294,17 @@ const UnggahLomba = () => {
                   Deskripsi lomba
                 </label>
                 <Textarea
+                  onChange={(e) => setDeskripsi(e.target.value)}
                   color="default"
                   className="mt-3 resize-none"
                   rows={20}
-                  placeholder="Lomba Web Design telah dibuka loh ayo buruan dafar!"></Textarea>
+                  placeholder="Lomba Web Design telah dibuka loh ayo buruan dafar!"
+                ></Textarea>
               </li>
               <li>
-                <Button color="primary" className="w-full">Unggah</Button>
+                <Button type="submit" color="primary" className="w-full">
+                  Unggah
+                </Button>
               </li>
             </ul>
           </div>
