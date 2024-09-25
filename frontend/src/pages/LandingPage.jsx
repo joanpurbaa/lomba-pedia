49ee4843a5c85c621bf07cdc2269748682c8a9d0
joanpurbaa <joanpurba562@gmail.com>
2024-09-26 05:30:43 +0700

    feat : create footer section on landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const LandingPage = () => {
   return (
     <>
-      <div className="w-full h-full flex flex-col items-center p-5">
+      <div className="w-full h-full flex flex-col items-center pt-5">
         <nav className="flex gap-x-20 bg-zinc-700 py-5 px-14 rounded-full">
           <li className="list-none text-white">
             <a href="#tentang">Tentang</a>
           </li>
           <li className="list-none text-white">
-            <a href="">FAQ</a>
+            <a href="#faq">FAQ</a>
           </li>
         </nav>
         <div className="flex flex-col items-center gap-y-6 py-20">
@@ -43,14 +43,14 @@ const LandingPage = () => {
           </div>
           <img className="w-72 rounded-lg" src="bob.jpg" alt="" />
         </div>
-        <div className="flex flex-col items-center w-full">
+        <div className="flex flex-col items-center w-full my-20">
           <h2 className="text-white text-4xl font-bold">
             Frequently Asked Question
           </h2>
           <h3 className="text-white text-base mt-3">
             Beberapa pertanyaan yang dapat membantu kamu!
           </h3>
-          <div className="w-[50%]">
+          <div id="faq" className="w-[50%]">
             <Accordion className="w-full mt-20 border-0" collapseAll>
               <Accordion.Panel>
                 <Accordion.Title className="bg-blue-600 hover:bg-blue-600 focus:ring-0 text-white">
@@ -93,6 +93,28 @@ const LandingPage = () => {
                 </Accordion.Content>
               </Accordion.Panel>
             </Accordion>
+          </div>
+        </div>
+        <div className="w-full flex justify-around items-start bg-zinc-800 py-10">
+          <div>
+            <h1 className="text-2xl font-bold text-white">Lomba Pedia</h1>
+            <p className="text-white mt-3">Temukan lomba yang kamu inginkan!</p>
+          </div>
+          <div>
+            <h1 className="text-2xl font-medium text-white">Menu</h1>
+            <nav className="mt-3">
+              <ul className="space-y-3">
+                <li>
+                  <a className="text-white" href="">Tentang</a>
+                </li>
+                <li>
+                  <a className="text-white" href="">FAQ</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div>
+            <h1 className="text-2xl font-medium text-white">Kontak</h1>
           </div>
         </div>
       </div>
