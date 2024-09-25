3485fb089f8071bf76112d1885f0d2616170f4b4
joanpurbaa <joanpurba562@gmail.com>
2024-09-25 15:47:00 +0700

    feat : adding FAQ acording

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import { Button } from "flowbite-react";
+import { Button, Accordion } from "flowbite-react";
 import { Link } from "react-router-dom";
 
 const LandingPage = () => {
@@ -39,14 +39,63 @@ const LandingPage = () => {
                 portal untuk memudahkan informasi seputar perlombaan
               </q>
             </p>
-            <p className="italic mt-10">- Joan The Creator</p>
+            <p className="italic mt-10">- Bob The Creator</p>
           </div>
-          <img className="w-72 rounded-lg" src="joan.jpg" alt="" />
+          <img className="w-72 rounded-lg" src="bob.jpg" alt="" />
         </div>
-        <div className="flex flex-col items-center">
-          <h2 className="text-white text-4xl font-bold">Frequently Asked Question</h2>
-          <h3 className="text-white text-base mt-3">Beberapa pertanyaan yang dapat membantu kamu!</h3>
-          <div></div>
+        <div className="flex flex-col items-center w-full">
+          <h2 className="text-white text-4xl font-bold">
+            Frequently Asked Question
+          </h2>
+          <h3 className="text-white text-base mt-3">
+            Beberapa pertanyaan yang dapat membantu kamu!
+          </h3>
+          <div className="w-[50%]">
+            <Accordion className="w-full mt-20 border-0" collapseAll>
+              <Accordion.Panel>
+                <Accordion.Title className="bg-blue-600 hover:bg-blue-600 focus:ring-0 text-white">
+                  Bagaimana cara menghapus lomba yang telah kita upload?
+                </Accordion.Title>
+                <Accordion.Content>
+                  <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    <span className="customUnderline">Pijar pelajar</span>{" "}
+                    merupakan platform belajar secara online khususnya skill
+                    bidang digital seperti coding, editing, data science,
+                    designing, dan banyak course digital lainnya.
+                  </p>
+                </Accordion.Content>
+              </Accordion.Panel>
+              <Accordion.Panel>
+                <Accordion.Title className="bg-blue-600 hover:bg-blue-600 focus:ring-0 text-white">
+                  Tujuan Pijar Pelajar?
+                </Accordion.Title>
+                <Accordion.Content>
+                  <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    Kami hadir untuk membantu banyak{" "}
+                    <span className="customUnderline">anak bangsa</span>,
+                    khususnya di bidang{" "}
+                    <span className="customUnderline">teknologi</span>, agar
+                    dapat bersaing di dunia{" "}
+                    <span className="customUnderline">modern</span>.
+                  </p>
+                </Accordion.Content>
+              </Accordion.Panel>
+              <Accordion.Panel>
+                <Accordion.Title className="bg-blue-600 hover:bg-blue-600 focus:ring-0 text-white">
+                  Benefit setelah belajar?
+                </Accordion.Title>
+                <Accordion.Content>
+                  <p className="mb-2 text-gray-500 dark:text-gray-400">
+                    Buat kamu yang belajar di{" "}
+                    <span className="customUnderline">Pijar Pelajar</span>,
+                    selain mendapat skill, dan relasi baru, kamu juga bakal
+                    dapat sertifikat kelulusan, dan bagi murid terbaik, maka
+                    secara otomatis akan mendapat pekerjaan di Start Up ini.
+                  </p>
+                </Accordion.Content>
+              </Accordion.Panel>
+            </Accordion>
+          </div>
         </div>
       </div>
     </>
